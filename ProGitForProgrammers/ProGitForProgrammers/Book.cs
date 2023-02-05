1080b66a53238559ef663a761a69b085406e5d94
yulin <yulin90625@gmail.com>
2023-02-05 16:41:32 +0800

    Add book class

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProGitForProgrammers
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
